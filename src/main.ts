b1f8edfc557cb179eb0ce7ec5f6658aba6125f76
sangwoong <sangwoong03@gmail.com>
2025-02-10 22:08:22 +0900

    feat: Swagger API 문서 세팅 - API 스펙, 엔드포인트, 요청 프로퍼티, 응답을 보고 테스트할 수 있는 swagger 환경을 추가합니다. - +hot reload 방식을 추가하여 로컬 환경에서 서버를 실행할 떄 변경 사항을 자동으로 반영할 수 있는 코드를 추가합니다.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
 import * as cookieParser from 'cookie-parser';
 
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exception/http-response-filter.exception';
 
 declare const module: any;
 
+async function swaggerSetup(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Footsb Backend API')
+    .setDescription('Footsb Backend API Documentation')
+    .setVersion('1.0.0')
+    .addTag('footsb')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  swaggerSetup(app);
 
   app.use(cookieParser());
 
