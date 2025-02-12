6a554651f0ecc334b070632a8b8f8f58e0b10e82
sangwoong <sangwoong03@gmail.com>
2025-02-12 12:57:09 +0900

    feat: Added Swagger decorator to Team API

--- src/modules/team/dto/find-one.dto.ts
+++ src/modules/team/dto/find-one.dto.ts
@@ -1,29 +1,72 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 import { TeamTypeEnum } from "src/common/eunm/team-type.enum";
 import { ITeamDetail } from "../interface/team-detail";
 import { GenderTypeEnum } from "src/common/eunm/gender-type.enum";
 import { LevelTypeEnum } from "src/common/eunm/level-type.enum";
 
 export class TeamDetailDto implements ITeamDetail {
+  @ApiProperty({ type: Number, example: 1 })
   id: number;
+
+  @ApiProperty({ type: String, example: '풋스비' })
   name: string;
+
+  @ApiProperty({ type: String, example: '풋스비 축구단 간단 소개' })
   summary: string;
+
+  @ApiProperty({ type: String, example: '풋스비 축구단 상세 소개' })
   description?: string | null;
+
+  @ApiProperty({ type: String, example: 'https://thumbnail-image.jpeg' })
   thumbnailImage?: string | null;
+
+  @ApiProperty({ type: Number, example: 3 })
   proCareerMembers: number;
+
+  @ApiProperty({ type: Number, example: 100 })
   manner: number;
+
+  @ApiProperty({ type: String, example: '월 수' })
   exerciseDays: string;
+
+  @ApiProperty({ type: String, example: '12~18시' })
   exerciseTime: string;
+
+  @ApiProperty({ type: String, example: '서울시 강남구' })
   exerciseAddress: string;
+
+  @ApiProperty({ type: Boolean, example: true })
   isRecruitingMembers: boolean;
+
+  @ApiProperty({ type: Number, example: 30 })
   maxMembers: number;
+
+  @ApiProperty({ enum: TeamTypeEnum, enumName: 'TeamTypeEnum', example: 'FOOTBALL | FOOTSAL' })
   teamType: `${TeamTypeEnum}`;
+
+  @ApiProperty({ enum: GenderTypeEnum, enumName: 'GenderTypeEnum', example: 'MALE | FEMALE | ANY' })
   genderType: `${GenderTypeEnum}`;
+
+  @ApiProperty({ enum: LevelTypeEnum, enumName: 'LevelTypeEnum', example: '하하하 | 하하 | 하 | 중 | 중하 | 중상 | 상' })
   levelType: `${LevelTypeEnum}`;
+
+  @ApiProperty({ type: String, example: '4-2-3-1' })
   formation: string;
+
+  @ApiProperty({ type: String, example: '풋스비 축구단 회장 이름' })
   ownerName: string;
+
+  @ApiProperty({ type: Number, example: 16 })
   memberCounts: number;
+
+  @ApiProperty({ type: Date, example: '2025-01-01' })
   createdAt: Date;
+
+  @ApiProperty({ type: Date, example: '2025-01-01' })
   updatedAt: Date;
+
+  @ApiProperty({ type: Date, example: '2025-01-01' })
   deletedAt?: Date;
 
     constructor(model: Record<string, any>) {
--- src/modules/team/team.controller.ts
+++ src/modules/team/team.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { TeamService } from './team.service';
 import { TeamListDto } from './dto/find-all.dto';
@@ -6,15 +7,28 @@ import { TeamDetailDto } from './dto/find-one.dto';
 import { ResponseFormatInterceptor } from 'src/common/interceptor/http-response-format.interceptor';
 
 @Controller('teams')
+@ApiTags('Team API')
 @UseInterceptors(ResponseFormatInterceptor)
 export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
+  @ApiOperation({ summary: '팀 목록 조회 API', description: '랜딩페이지에 접속할 때 모든 팀 목록을 조회합니다.' })
+  @ApiResponse({
+    status: 200,
+    description: '팀 목록을 반환합니다.',
+    type: TeamListDto
+  })
   @Get()
   async findAll(): Promise<TeamListDto[]> {
     return this.teamService.findAll();
   }
 
+  @ApiOperation({ summary: '팀 상세 조회 API', description: '팀의 세부 정보를 조회합니다.' })
+  @ApiResponse({
+    status: 200,
+    description: '팀 상제 정보를 반환합니다.',
+    type: TeamDetailDto
+  })
   @Get('/:id')
   async findOne(@Param('id') id: number): Promise<TeamDetailDto> {
     return this.teamService.findOne(id);
