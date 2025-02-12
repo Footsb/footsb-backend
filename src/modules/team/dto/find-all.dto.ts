b196ed470687b54a7b3e27622ff3319e50f3fdf5
sangwoong <sangwoong03@gmail.com>
2025-02-12 12:57:09 +0900

    feat: Added Swagger decorator to Team API

--- src/modules/team/dto/find-all.dto.ts
+++ src/modules/team/dto/find-all.dto.ts
@@ -1,17 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { GenderTypeEnum } from 'src/common/eunm/gender-type.enum';
 import { TeamTypeEnum } from 'src/common/eunm/team-type.enum';
 import { ITeamList } from '../interface/team-list.interface';
 
 export class TeamListDto implements ITeamList {
+  @ApiProperty({ type: Number, example: 1 })
   id: number;
+
+  @ApiProperty({ type: String, example: '풋스비' })
   name: string;
+
+  @ApiProperty({ type: String, example: '월 수' })
   exerciseDays: string;
+
+  @ApiProperty({ type: String, example: '12~18시' })
   exerciseTime: string;
+
+  @ApiProperty({ type: Number, example: 16 })
   memberCounts: number;
+
+  @ApiProperty({ type: Number, example: 30 })
   maxMembers: number;
+
+  @ApiProperty({ type: Boolean, example: true })
   isRecruitingMembers: boolean;
+
+  @ApiProperty({ enum: TeamTypeEnum, enumName: 'TeamTypeEnum', example: 'FOOTBALL | FOOTSAL' })
   teamType: `${TeamTypeEnum}`;
+
+  @ApiProperty({ enum: GenderTypeEnum, enumName: 'GenderTypeEnum', example: 'MALE | FEMALE | ANY' })
   genderType: `${GenderTypeEnum}`;
+
+  @ApiProperty({ type: Date, example: '2025-01-01' })
   createdAt: Date;
 
   constructor(model: Record<string, any>) {
