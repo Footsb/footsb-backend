39dbf657e048079f0797bf364bd596586cd7b54b
sangwoong <sangwoong03@gmail.com>
2025-02-12 12:42:10 +0900

    feat: Added Swagger decorator to User API

--- src/modules/user/dto/user-profile.dto.ts
+++ src/modules/user/dto/user-profile.dto.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export interface IUserProfile {
   id: number;
   name: string;
@@ -10,13 +12,28 @@ export interface IUserProfile {
 }
 
 export class UserProfile implements IUserProfile {
+  @ApiProperty({ type: Number, example: 1 })
   id: number;
+  
+  @ApiProperty({ type: String, example: '풋스비' })
   name: string;
+  
+  @ApiProperty({ type: String, example: 'footsb-admin@footsb.com' })
   email: string;
+
+  @ApiProperty({ type: String, example: '1234567890' })
   oAuthId: string;
+
+  @ApiProperty({ type: String, example: 'KAKAO' })
   oAuthType: string;
+
+  @ApiProperty({ type: String, example: 'https://image-url.jpeg' })
   profileImage: string;
+
+  @ApiProperty({ type: Date, example: '2025-01-01' })
   createdAt: Date;
+
+  @ApiProperty({ type: Date, example: '2025-01-01' })
   updatedAt: Date;
 
   constructor(user: IUserProfile) {
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,23 @@
 import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 import { UserService } from "./user.service";
 import { UserProfile } from "./dto/user-profile.dto";
 import { AuthGuard, RequestUser } from "../auth/guard/auth.guard";
 
 @Controller('users')
+@ApiTags('User API')
 export class UserController {
   constructor(
     private readonly userService: UserService
   ) {}
   
+  @ApiOperation({ summary: '접속 유저의 프로필 조회 API', description: '접속 유저의 액세스 토큰 검증 후 프로필 정보를 반환한다.' })
+  @ApiResponse({
+    status: 200,
+    description: '유저 프로필을 반환합니다.',
+    type: UserProfile
+  })
   @Get('profiles')
   @UseGuards(AuthGuard)
   async getUserProfile(
